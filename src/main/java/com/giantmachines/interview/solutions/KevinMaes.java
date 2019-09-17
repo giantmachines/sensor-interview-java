a72fee34c5b015088b870ea32a0fd652b5bef37c
Kevin Maes <kevin@tek212.com>
2019-09-17 16:22:58 -0400

    Refactor mode

--- src/main/java/com/giantmachines/interview/solutions/KevinMaes.java
+++ src/main/java/com/giantmachines/interview/solutions/KevinMaes.java
@@ -22,14 +22,14 @@ public class KevinMaes {
         return Arrays.stream(input).sorted().toArray();
     }
 
-    // public static Planet[] sortByName(Planet[] planets) {
-    // int[] sorted = new ArrayList<Planet>();
+    public static Planet[] sortByName(Planet[] planets) {
+        // int[] sorted = new ArrayList<Planet>();
 
-    // // return Collections.sort(planets, (a, b) -> a - b);
-    // // Planet[] planetsByName = Arrays.stream(planets).sorted().toArray();
-    // // System.out.println(String.valueOf(arr));
-    // // return planetsByName;
-    // }
+        // return Collections.sort(planets, (a, b) -> a - b);
+        // Planet[] planetsByName = Arrays.stream(planets).sorted().toArray();
+        // System.out.println(String.valueOf(arr));
+        return planets;
+    }
 
     public static double findMedian(int[] input) {
         int[] sorted = Arrays.stream(input).sorted().toArray();
@@ -48,38 +48,36 @@ public class KevinMaes {
     }
 
     public static double findMode(int[] input) {
-        int thisCount = 1;
-        int highestCount = 0;
-        int mostCommon = -1;
-
         // sort the array.
         int[] sorted = Arrays.stream(input).sorted().toArray();
 
+        int currentCount = 1;
+        int highestCount = 0;
+        int mostCommon = sorted[0];
+
         for (int i = 0; i < sorted.length; i++) {
             if (i == 0) {
-                mostCommon = sorted[i];
                 continue;
             }
 
             if (sorted[i] != sorted[i - 1]) {
                 // Number has changed.
-                thisCount = 1;
-
+                currentCount = 1;
             } else {
                 // Same as previous number.
-                thisCount = thisCount + 1;
+                currentCount = currentCount + 1;
             }
 
             // Evaluate this number's standing.
-            if (thisCount > highestCount) {
+            if (currentCount > highestCount) {
                 // New most common found.
-                highestCount = thisCount;
+                highestCount = currentCount;
                 mostCommon = sorted[i];
             }
         }
 
-        System.out.println(highestCount);
-        System.out.println(mostCommon);
+        // System.out.println(highestCount);
+        // System.out.println(mostCommon);
 
         return mostCommon;
     }
